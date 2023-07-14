1c1911eca6acbe5b2a96560ce297aaa24ee37a17
Reshmalb <reshma.lb27@gmail.com>
2023-07-14 23:34:46 +0530

    added  body-parser

--- backend-project3/app.js
+++ backend-project3/app.js
@@ -1,6 +1,9 @@
 var http = require("http");
 const express = require('express');
 const app = express();
+const bobyParser= require('body-parser');
+const bodyParser = require("body-parser");
+
 
 
 // const server = http.createServer(app);
@@ -12,17 +15,25 @@ const app = express();
 //     console.log("inside second middleware")
 //     res.send('<h1>This is second middleware</h1>')
 // })
+
+app.use(bodyParser.urlencoded({extended:false}))
+
 app.use('/',(req,res,next)=>{
     console.log("works always");
     next();
 })
-app.use('/addproducts',(req,res,next)=>{
+app.use('/add-products',(req,res,next)=>{
     console.log("inside first middleware")
-     res.send('<h1>This is add-product middleware</h1>');
+     res.send('<form action="/product" method="POST" > <input type="text" name="title"> <input type="text" name="size"><button type="submit">send</button> </form>');
 });
-app.use('/',(req,res,next)=>{
-    console.log("inside second middleware")
-    // res.send('<h1>This is second middleware</h1>')
+app.use('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
 })
 
-app.listen(3001);
+app.use('/',(req,res,next)=>{
+    console.log("inside second middleware")
+    res.send('<h1>This is second middleware</h1>')
+})
+
+app.listen(4000 );
