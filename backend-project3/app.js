66a97df3a697d99bbb671bd85010e6d8fb57fb3c
Reshmalb <reshma.lb27@gmail.com>
2023-07-15 00:14:18 +0530

    error404

--- backend-project3/app.js
+++ backend-project3/app.js
@@ -1,8 +1,8 @@
 var http = require("http");
-const express = require('express');
 const app = express();
-const bobyParser= require('body-parser');
 const bodyParser = require("body-parser");
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop')
 
 
 
@@ -17,23 +17,12 @@ const bodyParser = require("body-parser");
 // })
 
 app.use(bodyParser.urlencoded({extended:false}))
-
-app.use('/',(req,res,next)=>{
-    console.log("works always");
-    next();
-})
-app.use('/add-products',(req,res,next)=>{
-    console.log("inside first middleware")
-     res.send('<form action="/product" method="POST" > <input type="text" name="title"> <input type="text" name="size"><button type="submit">send</button> </form>');
-});
-app.use('/product',(req,res,next)=>{
-    console.log(req.body)
-    res.redirect('/')
+app.use(adminRoutes);
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>')
 })
 
-app.use('/',(req,res,next)=>{
-    console.log("inside second middleware")
-    res.send('<h1>This is second middleware</h1>')
-})
+app.use(shopRoutes);
+
 
 app.listen(4000 );
