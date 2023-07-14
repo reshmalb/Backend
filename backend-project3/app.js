3746f8377946950f320d0152cc6b85c535a543ff
Reshmalb <reshma.lb27@gmail.com>
2023-07-15 00:34:08 +0530

    filter routes

--- backend-project3/app.js
+++ backend-project3/app.js
@@ -1,5 +1,6 @@
 var http = require("http");
-const app = express();
+const express=require('express')
+const app=express();
 const bodyParser = require("body-parser");
 const adminRoutes=require('./routes/admin');
 const shopRoutes=require('./routes/shop')
@@ -17,12 +18,15 @@ const shopRoutes=require('./routes/shop')
 // })
 
 app.use(bodyParser.urlencoded({extended:false}))
-app.use(adminRoutes);
+// app.use(adminRoutes);
+app.use('/admin',adminRoutes);
+
+
+app.use('/shop',shopRoutes);
 app.use((req,res,next)=>{
     res.status(404).send('<h1>Page not found</h1>')
 })
 
-app.use(shopRoutes);
 
 
-app.listen(4000 );
+app.listen(5090);
